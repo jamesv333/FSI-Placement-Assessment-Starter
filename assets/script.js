c7654144943df24d2a7037532b9310ede47a9cdd
james <jvallon12@gmail.com>
2022-02-08 21:19:15 -0800

    i Assesment

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,78 @@
 // HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "James Vallon" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
-let gb = 0 // Gingerbread
+let gb = 0;
+
+function updateQuantity(Gingerbread) {
+    let gb = document.querySelector("#qty-gb");
+    gb.textContent = Gingerbread;
+}
+const gbdown = document.querySelector("#minus-gb");
+const gbup = document.querySelector("#add-gb");
+
+gbdown.addEventListener("click", function(e) {
+    if (gb > 0) {
+        gb--;
+        updateQuantity(gb);
+    }
+});
+
+gbup.addEventListener("click", function(e) {
+    if (gb  >= 0) {
+        gb++;
+        updateQuantity(gb);
+    }
+});
+
 let cc = 0 // Chocolate Chip
+
+function updateQuantity2(ChocolateChip) {
+    let cc = document.querySelector("#qty-cc");
+    cc.textContent = ChocolateChip;
+}
+const ccdown = document.querySelector("#minus-cc");
+const ccup = document.querySelector("#add-cc");
+
+ccdown.addEventListener("click", function(e) {
+    if (cc > 0) {
+        cc--;
+        updateQuantity2(cc);
+    }
+});
+
+ccup.addEventListener("click", function(e) {
+    if (cc  >= 0) {
+        cc++;
+        updateQuantity2(cc);
+    }
+});
+
 let sugar = 0 // Sugar Sprinkle
+
+function updateQuantity3(sugar) {
+    let sugar = document.querySelector("#qty-sugar");
+    sugar.textContent = ChocolateChip;
+}
+const sugardown = document.querySelector("#minus-sugar");
+const sugarup = document.querySelector("#add-sugar");
+
+sugardown.addEventListener("click", function(e) {
+    if (sugar > 0) {
+        sugar--;
+        updateQuantity3(sugar);
+    }
+});
+
+sugarup.addEventListener("click", function(e) {
+    if (sugar  >= 0) {
+        sugar++;
+        updateQuantity3(sugar);
+    }
+});
+
 
 // selecting the element with an id of credit
 const credit = document.querySelector('#credit')
